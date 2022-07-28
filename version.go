75cc68012ab9165273f08bfb173aab8f62bd580d
alallema <amelie@meilisearch.com>
2022-07-28 16:11:09 +0200

    Update version for the next release (v0.20.2)

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package meilisearch
 
 import "fmt"
 
-const VERSION = "0.20.0"
+const VERSION = "0.20.2"
 
 func GetQualifiedVersion() (qualifiedVersion string) {
 	return getQualifiedVersion(VERSION)
