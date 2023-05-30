59325a4a39964f92afb54f720e2e78c713572a6e
Amélie <alallema@users.noreply.github.com>
2023-05-30 18:09:29 +0200

    Update version.go
    Co-authored-by: Bruno Casali <brunoocasali@gmail.com>

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package meilisearch
 
 import "fmt"
 
-const VERSION = "0.24.1"
+const VERSION = "0.25.0"
 
 func GetQualifiedVersion() (qualifiedVersion string) {
 	return getQualifiedVersion(VERSION)
