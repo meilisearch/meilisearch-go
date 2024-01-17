a893ec8d9bdeb6372a25a740a49def1753bc98ea
Clémentine U. - curqui <clementine@meilisearch.com>
2024-01-17 12:17:28 +0100

    Update version.go (#508)

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package meilisearch
 
 import "fmt"
 
-const VERSION = "0.26.0"
+const VERSION = "0.26.1"
 
 func GetQualifiedVersion() (qualifiedVersion string) {
 	return getQualifiedVersion(VERSION)
