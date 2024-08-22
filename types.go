9ffd104344cec8850087c16869e9fe94b3ad9e2b
polyfloyd <floyd@polyfloyd.net>
2024-08-22 10:15:25 +0200

    Add Embedder.URL

--- types.go
+++ types.go
@@ -91,6 +91,7 @@ type Faceting struct {
 
 type Embedder struct {
 	Source           string `json:"source"`
+	URL              string `json:"url,omitempty"`
 	ApiKey           string `json:"apiKey,omitempty"`
 	Model            string `json:"model,omitempty"`
 	Dimensions       int    `json:"dimensions,omitempty"`
