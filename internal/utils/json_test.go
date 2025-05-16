5b7364e895ea248b5ac402bc7b96c11036b979ed
Javad <ja7ad@live.com>
2025-05-16 12:26:18 +0330

    feat: Add JSON marshal/unmarshal type definitions

---
+++ internal/utils/json_test.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type sampleStructure struct {
+	ImportantString string `json:"important_string"`
+}
+
+func Test_GolangJSONEncoder(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ss = &sampleStructure{
+			ImportantString: "Hello World",
+		}
+		importantString             = `{"important_string":"Hello World"}`
+		jsonEncoder     JSONMarshal = json.Marshal
+	)
+
+	raw, err := jsonEncoder(ss)
+	require.NoError(t, err)
+
+	require.Equal(t, string(raw), importantString)
+}
+
+func Test_DefaultJSONEncoder(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ss = &sampleStructure{
+			ImportantString: "Hello World",
+		}
+		importantString             = `{"important_string":"Hello World"}`
+		jsonEncoder     JSONMarshal = json.Marshal
+	)
+
+	raw, err := jsonEncoder(ss)
+	require.NoError(t, err)
+
+	require.Equal(t, string(raw), importantString)
+}
+
+func Test_DefaultJSONDecoder(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ss              sampleStructure
+		importantString               = []byte(`{"important_string":"Hello World"}`)
+		jsonDecoder     JSONUnmarshal = json.Unmarshal
+	)
+
+	err := jsonDecoder(importantString, &ss)
+	require.NoError(t, err)
+	require.Equal(t, "Hello World", ss.ImportantString)
+}
