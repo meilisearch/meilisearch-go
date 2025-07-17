5555a0a62a429f8b174f92211745b4ad2b515489
Javad <ja7ad@live.com>
2025-07-17 12:25:16 +0330

    test: Add tests for ErrorBody with encoder

--- error_test.go
+++ error_test.go
@@ -2,6 +2,7 @@ package meilisearch
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"testing"
 
@@ -46,4 +47,55 @@ func TestError_VersionErrorHintMessage(t *testing.T) {
 			require.Equal(t, tt.args.mockedError.Error()+". Hint: It might not be working because you're not up to date with the Meilisearch version that "+tt.args.request.functionName+" call requires", err.Error())
 		})
 	}
+}
+
+type mockEncoder struct{}
+
+func (m *mockEncoder) Encode(r io.Reader) (io.ReadCloser, error) {
+	return nil, nil
+}
+
+func (m *mockEncoder) Decode(data []byte, v interface{}) error {
+	msg, ok := v.(*APIError)
+	if !ok {
+		return fmt.Errorf("wrong type")
+	}
+	msg.Message = "mocked message"
+	msg.Code = "mocked code"
+	msg.Type = "mocked type"
+	msg.Link = "mocked link"
+	return nil
+}
+
+type failEncoder struct{}
+
+func (f *failEncoder) Encode(r io.Reader) (io.ReadCloser, error) {
+	return nil, nil
+}
+func (f *failEncoder) Decode(_ []byte, v interface{}) error {
+	return fmt.Errorf("decode failed")
+}
+
+func TestError_ErrorBody_WithEncoder(t *testing.T) {
+
+	err := &Error{
+		encoder: &mockEncoder{},
+	}
+	body := []byte(`{"message":"should not be used"}`)
+	err.ErrorBody(body)
+	require.Equal(t, "mocked message", err.MeilisearchApiError.Message)
+	require.Equal(t, "mocked code", err.MeilisearchApiError.Code)
+	require.Equal(t, "mocked type", err.MeilisearchApiError.Type)
+	require.Equal(t, "mocked link", err.MeilisearchApiError.Link)
+
+	err2 := &Error{
+		encoder: &failEncoder{},
+	}
+	body2 := []byte(`{"message":"should not be used"}`)
+	err2.ErrorBody(body2)
+	// Should not set MeilisearchApiError fields
+	require.Empty(t, err2.MeilisearchApiError.Message)
+	require.Empty(t, err2.MeilisearchApiError.Code)
+	require.Empty(t, err2.MeilisearchApiError.Type)
+	require.Empty(t, err2.MeilisearchApiError.Link)
 }
