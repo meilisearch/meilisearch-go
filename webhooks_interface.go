00af9d7aa823f8885c507def306678a37ce5b551
elyar <elyarnejati@gmail.com>
2025-08-16 00:01:19 +0330

    feat: add webhook reader methods

--- webhooks_interface.go
+++ webhooks_interface.go
@@ -11,4 +11,13 @@ type WebhookManager interface {
 	AddWebhookWithContext(ctx context.Context, params *AddWebhookQuery) (*Webhook, error)
 }
 
-type WebhookReader interface{}
+type WebhookReader interface{
+	// ListWebhooks lists all the webhooks.
+	ListWebhooks() (*WebhookResults, error)
+	// ListWebhooksWithContext lists all the webhooks with context.
+	ListWebhooksWithContext(ctx context.Context) (*WebhookResults, error)
+	// GetWebhook gets a webhook by uuid.
+	GetWebhook(uuid string) (*Webhook, error)
+	// GetWebhookWithContext gets a webhook by uuid with a context.
+	GetWebhookWithContext(ctx context.Context, uuid string) (*Webhook, error)
+}
