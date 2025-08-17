47d975b72ce5da48256e21349821997f17dbd329
elyar <elyarnejati@gmail.com>
2025-08-17 22:01:36 +0330

    update: add networks interfaces & methods

---
+++ networks_interface.go
@@ -0,0 +1,21 @@
+package meilisearch
+
+import "context"
+
+type NetworksManager interface {
+	NetworksReader
+
+	// UpdateNetwork updates the network object.
+	UpdateNetwork(params *Network) (*Network, error)
+
+	// UpdateNetworkWithContext updates the network object with a context.
+	UpdateNetworkWithContext(ctx context.Context, params *Network) (*Network, error)
+}
+
+type NetworksReader interface {
+	// GetNetwork gets the current value of the instance’s network object.
+	GetNetwork() (*Network, error)
+
+	// GetNetworkWithContext gets the current value of the instance’s network object with a context.
+	GetNetworkWithContext(ctx context.Context) (*Network, error)
+}
