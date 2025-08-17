5512e3586c40fa5868b0364f4997ce9c738f5207
elyar <elyarnejati@gmail.com>
2025-08-17 22:41:25 +0330

    test: add integration tests for network

--- networks_interface.go
+++ networks_interface.go
@@ -6,9 +6,11 @@ type NetworksManager interface {
 	NetworksReader
 
 	// UpdateNetwork updates the network object.
+	// Updates are partial; only the provided fields are updated.
 	UpdateNetwork(params *Network) (*Network, error)
 
 	// UpdateNetworkWithContext updates the network object with a context.
+	// Updates are partial; only the provided fields are updated.
 	UpdateNetworkWithContext(ctx context.Context, params *Network) (*Network, error)
 }
 
