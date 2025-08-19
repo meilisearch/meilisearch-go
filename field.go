cb580c1e90cb40505d45e5c1d5b0b23952727b52
Javad <ja7ad@live.com>
2025-08-19 09:14:59 +0330

    refactor: rename pointer utility functions for clarity

--- field.go
+++ field.go
@@ -2,20 +2,20 @@ package meilisearch
 
 import "time"
 
-// Int returns a pointer to the given int value.
-func Int(v int) *int { return &v }
+// IntPtr returns a pointer to the given int value.
+func IntPtr(v int) *int { return &v }
 
-// Int64 returns a pointer to the given int64 value.
-func Int64(v int64) *int64 { return &v }
+// Int64Ptr returns a pointer to the given int64 value.
+func Int64Ptr(v int64) *int64 { return &v }
 
-// Bool returns a pointer to the given bool value.
-func Bool(v bool) *bool { return &v }
+// BoolPtr returns a pointer to the given bool value.
+func BoolPtr(v bool) *bool { return &v }
 
-// Float returns a pointer to the given float64 value.
-func Float(v float64) *float64 { return &v }
+// FloatPtr returns a pointer to the given float64 value.
+func FloatPtr(v float64) *float64 { return &v }
 
-// String returns a pointer to the given string value.
-func String(v string) *string { return &v }
+// StringPtr returns a pointer to the given string value.
+func StringPtr(v string) *string { return &v }
 
-// Time returns a pointer to the given time.Time value.
-func Time(v time.Time) *time.Time { return &v }
+// TimePtr returns a pointer to the given time.Time value.
+func TimePtr(v time.Time) *time.Time { return &v }
--- field_test.go
+++ field_test.go
@@ -9,42 +9,42 @@ import (
 
 func TestInt(t *testing.T) {
 	v := 42
-	ptr := Int(v)
-	assert.NotNil(t, ptr, "Int returned nil pointer")
-	assert.Equal(t, v, *ptr, "Int returned wrong value")
+	ptr := IntPtr(v)
+	assert.NotNil(t, ptr, "IntPtr returned nil pointer")
+	assert.Equal(t, v, *ptr, "IntPtr returned wrong value")
 }
 
 func TestInt64(t *testing.T) {
 	v := int64(42)
-	ptr := Int64(v)
-	assert.NotNil(t, ptr, "Int64 returned nil pointer")
-	assert.Equal(t, v, *ptr, "Int64 returned wrong value")
+	ptr := Int64Ptr(v)
+	assert.NotNil(t, ptr, "Int64Ptr returned nil pointer")
+	assert.Equal(t, v, *ptr, "Int64Ptr returned wrong value")
 }
 
 func TestBool(t *testing.T) {
 	v := true
-	ptr := Bool(v)
-	assert.NotNil(t, ptr, "Bool returned nil pointer")
-	assert.Equal(t, v, *ptr, "Bool returned wrong value")
+	ptr := BoolPtr(v)
+	assert.NotNil(t, ptr, "BoolPtr returned nil pointer")
+	assert.Equal(t, v, *ptr, "BoolPtr returned wrong value")
 }
 
 func TestFloat(t *testing.T) {
 	v := 3.14
-	ptr := Float(v)
-	assert.NotNil(t, ptr, "Float returned nil pointer")
-	assert.Equal(t, v, *ptr, "Float returned wrong value")
+	ptr := FloatPtr(v)
+	assert.NotNil(t, ptr, "FloatPtr returned nil pointer")
+	assert.Equal(t, v, *ptr, "FloatPtr returned wrong value")
 }
 
 func TestString(t *testing.T) {
 	v := "hello"
-	ptr := String(v)
-	assert.NotNil(t, ptr, "String returned nil pointer")
-	assert.Equal(t, v, *ptr, "String returned wrong value")
+	ptr := StringPtr(v)
+	assert.NotNil(t, ptr, "StringPtr returned nil pointer")
+	assert.Equal(t, v, *ptr, "StringPtr returned wrong value")
 }
 
 func TestTime(t *testing.T) {
 	v := time.Date(2020, 1, 2, 3, 4, 5, 6, time.UTC)
-	ptr := Time(v)
-	assert.NotNil(t, ptr, "Time returned nil pointer")
-	assert.True(t, ptr.Equal(v), "Time returned wrong value")
+	ptr := TimePtr(v)
+	assert.NotNil(t, ptr, "TimePtr returned nil pointer")
+	assert.True(t, ptr.Equal(v), "TimePtr returned wrong value")
 }
