78e9ed2c193e91ee88bde1759f61120575f7d32a
Javad Rajabzadeh <ja7ad@live.com>
2025-09-04 22:51:43 +0330

    Bump version from 0.33.2 to 0.33.3

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package meilisearch
 
 import "fmt"
 
-const VERSION = "0.33.2"
+const VERSION = "0.33.3"
 
 func GetQualifiedVersion() (qualifiedVersion string) {
 	return getQualifiedVersion(VERSION)
