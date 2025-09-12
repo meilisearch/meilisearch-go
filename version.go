0f62d15e5f8cce0bb80b054d1f3d6e051430fb00
Javad Rajabzadeh <ja7ad@live.com>
2025-09-12 12:35:49 +0330

    Update version for the next release (v0.34.0)

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package meilisearch
 
 import "fmt"
 
-const VERSION = "0.33.3"
+const VERSION = "0.34.0"
 
 func GetQualifiedVersion() (qualifiedVersion string) {
 	return getQualifiedVersion(VERSION)
