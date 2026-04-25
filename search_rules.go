388f2980037d380a31376d611e3264f3a66bb55f
elyar <elyarnejati@gmail.com>
2026-04-25 12:33:14 +0330

    feat: add List method for search rules

--- search_rules.go
+++ search_rules.go
@@ -6,6 +6,29 @@ import (
 	"net/http"
 )
 
+func (m *meilisearch) List(params *SearchRulesParams) (*SearchRulesResults, error) {
+	return m.ListWithContext(context.Background(), params)
+}
+
+func (m *meilisearch) ListWithContext(ctx context.Context, params *SearchRulesParams) (*SearchRulesResults, error) {
+	resp := new(SearchRulesResults)
+
+	req := &internalRequest{
+		endpoint:            "/dynamic-search-rules",
+		method:              http.MethodPost,
+		withRequest:         params,
+		withResponse:        resp,
+		acceptedStatusCodes: []int{http.StatusOK},
+		functionName:        "ListSearchRules",
+	}
+
+	if err := m.client.executeRequest(ctx, req); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
 func (m *meilisearch) Get(uid string) (*SearchRule, error) {
 	return m.GetWithContext(context.Background(), uid)
 }
@@ -18,7 +41,7 @@ func (m *meilisearch) GetWithContext(ctx context.Context, uid string) (*SearchRu
 		method:              http.MethodGet,
 		withRequest:         nil,
 		withResponse:        resp,
-		acceptedStatusCodes: []int{http.StatusNoContent},
+		acceptedStatusCodes: []int{http.StatusOK},
 		functionName:        "GetSearchRule",
 	}
 
