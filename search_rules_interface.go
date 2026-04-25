2a0965df625bdb8e36778b83bfba598051c1eb6b
elyar <elyarnejati@gmail.com>
2026-04-25 12:14:58 +0330

    feat: add Get method to SearchRule

--- search_rules_interface.go
+++ search_rules_interface.go
@@ -17,4 +17,13 @@ type SearchRulesManager interface {
 }
 
 type SearchRulesReader interface {
+	// Get retrieve a single dynamic search rule by its unique identifier.
+	//
+	// docs: https://www.meilisearch.com/docs/reference/api/dynamic-search-rules/get-a-dynamic-search-rule
+	Get(uid string) (*SearchRule, error)
+
+	// GetWithContext retrieve a single dynamic search rule by its unique identifier with a context.
+	//
+	// docs: https://www.meilisearch.com/docs/reference/api/dynamic-search-rules/get-a-dynamic-search-rule
+	GetWithContext(ctx context.Context, uid string) (*SearchRule, error)
 }
