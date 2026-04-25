6143f2b3bccf255f27aba8a506778c9973e0d4cf
elyar <elyarnejati@gmail.com>
2026-04-25 13:37:46 +0330

    feat: add Update method for SearchRules

--- search_rules.go
+++ search_rules.go
@@ -6,6 +6,29 @@ import (
 	"net/http"
 )
 
+func (m *meilisearch) Update(uid string, params *SearchRulesRequest) (*SearchRule, error) {
+	return m.UpdateWithContext(context.Background(), uid, params)
+}
+
+func (m *meilisearch) UpdateWithContext(ctx context.Context, uid string, params *SearchRulesRequest) (*SearchRule, error) {
+	resp := new(SearchRule)
+
+	req := &internalRequest{
+		endpoint:            fmt.Sprintf("/dynamic-search-rules/%s", uid),
+		method:              http.MethodPatch,
+		withRequest:         params,
+		withResponse:        resp,
+		acceptedStatusCodes: []int{http.StatusOK},
+		functionName:        "UpdateSearchRule",
+	}
+
+	if err := m.client.executeRequest(ctx, req); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
 func (m *meilisearch) List(params *SearchRulesParams) (*SearchRulesResults, error) {
 	return m.ListWithContext(context.Background(), params)
 }
