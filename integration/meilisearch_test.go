d1fda97631d618c1bbda1123732e5966dba44209
elyar <elyarnejati@gmail.com>
2026-06-30 22:02:57 +0330

    chore: update code samples and add docs for methods

--- integration/meilisearch_test.go
+++ integration/meilisearch_test.go
@@ -2786,8 +2786,6 @@ func TestRenderTemplate(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, exFeature.RenderRoute)
 
-	c.ExperimentalFeatures().SetMultiModal(true).Update()
-
 	indexUid := "movies"
 	setupMovieIndex(t, c, indexUid)
 
